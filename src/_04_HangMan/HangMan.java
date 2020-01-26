a2d62fc3ecc53cf46c840a101cd43351fa73cae6
League Student <league@iMac-5.attlocal.net>
2020-01-25 17:25:29 -0800

    Hangman(IP)
    Now accepts char input and replaces underscores with chars. Also has Lives, which are in progress.

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -15,7 +15,13 @@ public class HangMan implements KeyListener{
 	static JPanel P = new JPanel();
 	static JLabel L = new JLabel();
 	
-	static int words;
+	static int numWords;
+	static int lives = 10;
+	static int numChar;
+	
+	static String spaces = "";
+	static String word;
+	
 	
 	static Stack<String> strings = new Stack<String>();
 	
@@ -23,10 +29,10 @@ public class HangMan implements KeyListener{
 		HangMan h = new HangMan();
 		Utilities utilities = new Utilities();
 		
-		String wordAmount = JOptionPane.showInputDialog("Welcome to Hangman!" + "/n" + "How many words would you like to guess?");
+		String wordAmount = JOptionPane.showInputDialog("Welcome to Hangman!" + "\n" + "How many words would you like to guess?");
 		
-		words = Integer.parseInt(wordAmount);
-		for(int r =0 ; r < words; r++) {
+		numWords = Integer.parseInt(wordAmount);
+		for(int r =0 ; r < numWords; r++) {
 		strings.push(utilities.readRandomLineFromFile("dictionary.txt"));
 		}
 		h.setup();
@@ -35,6 +41,13 @@ public class HangMan implements KeyListener{
 	void setup() {
 		J.add(P);
 		P.add(L);
+		numChar = strings.get(0).length();
+		word = strings.get(0);
+		System.out.println(word);
+		for(int i = 0; i < numChar; i++) {
+			spaces =  spaces + "_ ";
+		}
+		L.setText(spaces + "Lives left: " + lives);
 		J.pack();
 		J.setSize(500, 400);
 		J.setLocation(750, 300);
@@ -51,7 +64,18 @@ public class HangMan implements KeyListener{
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		char c = e.getKeyChar();
+		for(int i = 0; i < word.length(); i++) {
+			if(c == word.charAt(i)) {
+				spaces = spaces.substring(0, i*2) + word.charAt(i) + spaces.substring(i*2+1 , spaces.length());
+				L.setText(spaces + "Lives left: " + lives);
+			} else if(c != word.charAt(i)) {
+				lives--;
+			}
+			
+		}
 		
+		//make sure to set numChar to the new word once someone has filled in all the blanks
 	}
 
 	@Override
